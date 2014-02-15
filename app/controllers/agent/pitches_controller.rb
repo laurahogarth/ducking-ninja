a434277fbd4637358c3d636a026d88ea39df15bf
Mikey Hogarth <mikehogarth20@hotmail.com>
2014-02-15 10:59:41 +0000

    Pitch Controller tests

--- app/controllers/agent/pitches_controller.rb
+++ app/controllers/agent/pitches_controller.rb
@@ -3,29 +3,28 @@ class Agent::PitchesController < ApplicationController
   load_and_authorize_resource
   skip_load_resource :only => :create
   before_filter :authenticate_agent!
-  #before_action :set_pitch, only: [:show, :edit, :update, :destroy]
-  before_action :set_holiday, :only => [:show, :edit, :update, :new, :create, :destroy]
+  before_action :set_holiday, :only => [:show, :edit, :update, :new, :create]
 
   
-  # GET /pitches
+  # GET agent/pitches
   def index
     @pitches = current_agent.pitches
   end
 
-  # GET /pitches/1
+  # GET agent/holidays/1/pitches/1
   def show
   end
 
-  # GET /pitches/new
+  # GET agent/holidays/1/pitches/new
   def new
     @pitch = @holiday.pitches.build
   end
 
-  # GET /pitches/1/edit
+  # GET agent/holidays/1/pitches/1/edit
   def edit
   end
 
-  # POST /pitches
+  # POST agent/holidays/1/pitches
   def create
     @pitch = @holiday.pitches.build(pitch_params.merge(:agent_id => current_agent.id))
 
@@ -36,7 +35,7 @@ class Agent::PitchesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pitches/1
+  # PATCH/PUT agent/holidays/1/pitches/1
   def update
     if @pitch.update(pitch_params)
       redirect_to [:agent, @holiday, @pitch], notice: 'Pitch was successfully updated.'
@@ -45,18 +44,14 @@ class Agent::PitchesController < ApplicationController
     end
   end
 
-  # DELETE /pitches/1
+  # DELETE agent/holidays/1/pitches/1
   def destroy
     @pitch.destroy
     redirect_to agent_pitches_url, notice: 'Pitch was successfully destroyed.'
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    #def set_pitch
-    #  @pitch = Pitch.find(params[:id])
-    #end
 
+  private
     def set_holiday
       @holiday = Holiday.find(params[:holiday_id])
     end
--- spec/controllers/agent/pitches_controller_spec.rb
+++ spec/controllers/agent/pitches_controller_spec.rb
@@ -2,44 +2,40 @@ require 'spec_helper'
 
 describe Agent::PitchesController do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Pitch. As you add validations to Pitch, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) { { "min" => "1" } }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PitchesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  before do
+    @agent = FactoryGirl.create(:agent)
+    @holiday = FactoryGirl.create(:holiday)
+    @pitch = FactoryGirl.create(:pitch, :holiday => @holiday, :agent => @agent)
+    sign_in :agent, @agent
+  end 
 
   describe "GET index" do
     it "assigns all pitches as @pitches" do
-      pitch = Pitch.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:pitches)).to eq([pitch])
+      get :index
+      expect(assigns(:pitches)).to eq([@pitch])
     end
   end
 
   describe "GET show" do
     it "assigns the requested pitch as @pitch" do
-      pitch = Pitch.create! valid_attributes
-      get :show, {:id => pitch.to_param}, valid_session
-      expect(assigns(:pitch)).to eq(pitch)
+      get :show, {:holiday_id => @holiday, :id => @pitch}
+      expect(assigns(:pitch)).to eq(@pitch)
+      expect(assigns(:holiday)).to eq(@holiday)
     end
   end
 
   describe "GET new" do
     it "assigns a new pitch as @pitch" do
-      get :new, {}, valid_session
-      expect(assigns(:pitch)).to be_a_new(Pitch)
+      get :new, {:holiday_id => @holiday }
+      expect(assigns(:holiday)).to eq(@holiday)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested pitch as @pitch" do
-      pitch = Pitch.create! valid_attributes
-      get :edit, {:id => pitch.to_param}, valid_session
-      expect(assigns(:pitch)).to eq(pitch)
+      get :edit, {:holiday_id => @holiday, :id => @pitch}
+      expect(assigns(:pitch)).to eq(@pitch)
+      expect(assigns(:holiday)).to eq(@holiday)
     end
   end
 
@@ -47,95 +43,40 @@ describe Agent::PitchesController do
     describe "with valid params" do
       it "creates a new Pitch" do
         expect {
-          post :create, {:pitch => valid_attributes}, valid_session
+          @pitch.holiday = FactoryGirl.create(:holiday)
+          post :create, {:holiday_id => @holiday, :pitch => @pitch.attributes }
         }.to change(Pitch, :count).by(1)
       end
 
-      it "assigns a newly created pitch as @pitch" do
-        post :create, {:pitch => valid_attributes}, valid_session
-        expect(assigns(:pitch)).to be_a(Pitch)
+      it "assigns a newly created pitch as @pitch and redirects" do
+        @pitch.holiday = FactoryGirl.create(:holiday)
+        post :create, {:holiday_id => @holiday, :pitch => @pitch.attributes }
         expect(assigns(:pitch)).to be_persisted
-      end
-
-      it "redirects to the created pitch" do
-        post :create, {:pitch => valid_attributes}, valid_session
-        expect(response).to redirect_to(Pitch.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved pitch as @pitch" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Pitch.any_instance.stub(:save).and_return(false)
-        post :create, {:pitch => { "min" => "invalid value" }}, valid_session
-        expect(assigns(:pitch)).to be_a_new(Pitch)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Pitch.any_instance.stub(:save).and_return(false)
-        post :create, {:pitch => { "min" => "invalid value" }}, valid_session
-        expect(response).to render_template("new")
+        expect(response).to redirect_to([:agent, @holiday, assigns[:pitch]])
       end
     end
   end
 
+
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested pitch" do
-        pitch = Pitch.create! valid_attributes
-        # Assuming there are no other pitches in the database, this
-        # specifies that the Pitch created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        expect_any_instance_of(Pitch).to receive(:update).with({ "min" => "1" })
-        put :update, {:id => pitch.to_param, :pitch => { "min" => "1" }}, valid_session
-      end
-
-      it "assigns the requested pitch as @pitch" do
-        pitch = Pitch.create! valid_attributes
-        put :update, {:id => pitch.to_param, :pitch => valid_attributes}, valid_session
-        expect(assigns(:pitch)).to eq(pitch)
-      end
-
-      it "redirects to the pitch" do
-        pitch = Pitch.create! valid_attributes
-        put :update, {:id => pitch.to_param, :pitch => valid_attributes}, valid_session
-        expect(response).to redirect_to(pitch)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the pitch as @pitch" do
-        pitch = Pitch.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Pitch.any_instance.stub(:save).and_return(false)
-        put :update, {:id => pitch.to_param, :pitch => { "min" => "invalid value" }}, valid_session
-        expect(assigns(:pitch)).to eq(pitch)
-      end
-
-      it "re-renders the 'edit' template" do
-        pitch = Pitch.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Pitch.any_instance.stub(:save).and_return(false)
-        put :update, {:id => pitch.to_param, :pitch => { "min" => "invalid value" }}, valid_session
-        expect(response).to render_template("edit")
+        put :update, { :holiday_id => @holiday, :id => @pitch, :pitch => @pitch.attributes }
+        expect(assigns[:pitch]).to be_persisted
       end
     end
   end
 
   describe "DELETE destroy" do
     it "destroys the requested pitch" do
-      pitch = Pitch.create! valid_attributes
       expect {
-        delete :destroy, {:id => pitch.to_param}, valid_session
+        delete :destroy, {:holiday_id => @holiday, :id => @pitch} 
       }.to change(Pitch, :count).by(-1)
     end
 
     it "redirects to the pitches list" do
-      pitch = Pitch.create! valid_attributes
-      delete :destroy, {:id => pitch.to_param}, valid_session
-      expect(response).to redirect_to(pitches_url)
+      delete :destroy, {:holiday_id => @holiday, :id => @pitch}
+      expect(response).to redirect_to(agent_pitches_url)
     end
   end
 
