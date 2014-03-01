d288c5c3421113f6803f93e774e237cb36ad7c50
Mikey Hogarth <mikehogarth20@hotmail.com>
2014-03-01 11:37:12 +0000

    Tarting up home page

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -1,5 +1,10 @@
 module HtmlHelper
 
+
+  def placekitten(width, height)
+    image_tag "http://placekitten.com/g/#{width}/#{height}"
+  end
+
   def nice_boolean(bool)
     if bool
       content_tag :span, "Yes", :class => [:success, :label]
